4bd6916a65dd36619254215cf3de1410414b642c
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-01-19 14:24:13 -0300

    fix: product and category module connections updated

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categories } from './entities/category.entity';
+import { CategoriesService } from './services/category.service';
+import { CategoriesController } from './controllers/category.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Categories])],
-  providers: [],
-  controllers: [],
-  exports: [],
+  providers: [CategoriesService],
+  controllers: [CategoriesController],
+  exports: [CategoriesService],
 })
 export class CategoryModule {}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Products } from './entities/product.entity';
 import { CategoryModule } from '../category/category.module';
+import { ProductsController } from './controllers/product.controller';
+import { ProductsService } from './services/product.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Products]), CategoryModule],
-  providers: [],
-  controllers: [],
+  providers: [ProductsController],
+  controllers: [ProductsService],
   exports: [],
 })
 export class ProductModule {}
